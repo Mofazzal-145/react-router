6859a74c59cf2ad162ee819a9bf4ae4b6e466991
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-29 16:25:33 +0600

    declaring route

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3>The page you are looking for not available</h3>
+            <h5>404</h5>
+        </div>
+    );
+};
+
+export default NotFound;
