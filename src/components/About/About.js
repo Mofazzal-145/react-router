7f7b76fde41868e79d24d3818b95b0512171873e
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-29 15:19:21 +0600

    added many components

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>Tell me about yourself</h2>
+            <p>Naila job pabina???????</p>
+            <p><small>Wanna talk to me.</small></p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h2>Hi Friends.</h2>
+            <p><small>Let's go outside</small></p>
+        </div>
+    );
+};
+
+export default Friends;
