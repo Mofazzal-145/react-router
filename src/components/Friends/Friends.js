99230d50d45ab9504ec9c96e2ec690781a0587f5
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-30 22:17:18 +0600

    add Friend component

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const [friends, setFriends] = useState([]);
+    useEffect( ()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setFriends(data))
+    }, [])
     return (
         <div>
             <h2>Hi Friends.</h2>
-            <p><small>Let's go outside</small></p>
+            <p><small>Let's go outside...:{friends.length}</small></p>
+            {
+                friends.map(friend => <Friend
+                key={friend.id} 
+                friend={friend}
+                ></Friend>)
+            }
         </div>
     );
 };
