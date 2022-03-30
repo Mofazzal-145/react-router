e5e21d94f7f7f65c45174c98a90b01eb33cbaf9a
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-30 21:05:15 +0600

    few changes

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h3>The page you are looking for not available</h3>
+            <h3>The page you are looking for not available !!!!!</h3>
             <h5>404</h5>
         </div>
     );
