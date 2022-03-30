6a7ce511d9a48b9fd26ee8ef4977d85f92404dd9
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-30 23:06:06 +0600

    added custom-link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <div>
             <h1>Welcome to my fancy routing website!!!</h1>
             <nav>
-                <Link to="/">Home</Link>
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/friends">Friends</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
+
+                {/* <Link to="/">Home</Link>
                 <Link to="/friends">Friends</Link>
-                <Link to="/about">About</Link>
+                <Link to="/about">About</Link> */}
             </nav>
         </div>
     );
